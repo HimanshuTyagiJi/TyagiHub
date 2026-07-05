885a294641fd2dba2f8e0cc22cf9f05ff1e77807
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2026-07-05 16:29:49 +0530

    Update import paths to include .js extension

--- assets/js/stock/components/AdminPanel.jsx
+++ assets/js/stock/components/AdminPanel.jsx
@@ -11,8 +11,8 @@ import {
   Package, 
   FolderSync 
 } from "lucide-react";
-import { SCRIPT_URL, fetchGAS } from "/assets/js/stock/services/api";
-import { extractDriveId } from "/assets/js/stock/utils/drm";
+import { SCRIPT_URL, fetchGAS } from "/assets/js/stock/services/api.js";
+import { extractDriveId } from "/assets/js/stock/utils/drm.js";
 
 export default function AdminPanel({ 
   requests, 
