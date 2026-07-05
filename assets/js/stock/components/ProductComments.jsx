418730c044e0e25fcf6ba6e7100983cd7b1aac53
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2026-07-05 16:02:49 +0530

    Update ProductComments.jsx

--- assets/js/stock/components/ProductComments.jsx
+++ assets/js/stock/components/ProductComments.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { SCRIPT_URL } from "../services/api";
+import { SCRIPT_URL } from "../services/api"; 
 
 export default function ProductComments({ product, user, openLogin }) {
   const [comments, setComments] = useState([]);
