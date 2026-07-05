59ace5816242badc2abdbaf2e82e02ac3caa36ae
HimanshuTyagiJi <Officialhimanshutyagi@gmail.com>
2026-07-05 15:46:45 +0530

    Create main.jsx

---
+++ assets/js/stock/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx"; // <-- .jsx extension yahan lagana zaroori hai bsdk!
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
